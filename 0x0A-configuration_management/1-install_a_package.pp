c91006db80b504b4a6a27600cd3b6dff482eb54e
Solomon-dev98 <asapnino01@gmail.com>
2024-07-04 13:55:24 +0100

    fix: used fewer lines of codes

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,5 @@
 # install a package, flask from pip3
-
-package {'python3-pip':
-    ensure => installed,
-}
-
-
 package { 'flask':
     ensure   => '2.1.0',
     provider => 'pip3',
-    require  => Package['python3-pip'],
 }
