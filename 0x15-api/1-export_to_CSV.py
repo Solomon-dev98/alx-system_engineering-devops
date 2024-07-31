72c6c7d42d37d204ccb5aa59cdde0b704c8fbe40
Solomon-dev98 <asapnino01@gmail.com>
2024-07-31 13:17:20 +0100

    fixed errors

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,51 +1,24 @@
 #!/usr/bin/python3
-"""Implementation of a python script that returns information
-    using a REST api"""
-import csv
+"""Accessing a REST API for todo lists of employees"""
+
 import requests
 import sys
 
 
-def get_employee_todo_progress(employee_id):
-    # Base URL for the API
-    base_url = "https://jsonplaceholder.typicode.com"
+if __name__ == '__main__':
+    employeeId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
 
-    # Endpoint for user and todos
-    user_url = f"{base_url}/users/{employee_id}"
-    todos_url = f"{base_url}/todos?userId={employee_id}"
+    response = requests.get(url)
+    username = response.json().get('username')
 
-    # Fetch user data
-    user_response = requests.get(user_url)
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
 
-    # Fetch todos data
-    todos_response = requests.get(todos_url)
-    if todos_response.status_code != 200:
-        print("Error: Could not fetch TODOS data.")
-        return
-
-    # Parse JSON data
-    user_data = user_response.json()
-    todos_data = todos_response.json()
-
-    # Extract necessary information
-    employee_name = user_data.get("name")
-    total = len(todos_data)
-    done = [todo for todo in todos_data if todo.get("completed")]
-    d_tasks = len(done)
-
-    # Export to CSV
-    csv_filename = f"{employee_id}.csv"
-    with open(csv_filename, mode='w', newline='') as csv_file:
-        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-        for task in todos_data:
-            writer.writerow([
-                employee_id,
-                employee_name,
-                task.get("completed"),
-                task.get("title")
-            ])
-
-
-if __name__ == "__main__":
-    employee_id = int(sys.argv[1])
-    get_employee_todo_progress(employee_id)
+    with open('{}.csv'.format(employeeId), 'w') as file:
+        for task in tasks:
+            file.write('"{}","{}","{}","{}"\n'
+                       .format(employeeId, username, task.get('completed'),
+                               task.get('title')))
